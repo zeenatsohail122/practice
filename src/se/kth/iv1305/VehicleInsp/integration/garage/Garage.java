6480adecb530ce8aeaa18572b6194710a6821811
ZeenatAli <ZeenatAli@zeenats-mbp.lan>
2017-05-29 14:06:24 +0200

    Exception Handling

--- src/se/kth/iv1305/VehicleInsp/integration/garage/Garage.java
+++ src/se/kth/iv1305/VehicleInsp/integration/garage/Garage.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package se.kth.iv1305.VehicleInsp.integration.garage;
 
 public class Garage {
